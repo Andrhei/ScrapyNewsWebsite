e1b0c81f531ef09d3b7e20dcaf4b27f421934fd4
Andrhei <andrheihqs@gmail.com>
2023-04-08 23:18:58 -0300

    Scrapy final

--- ScrapyNewsWebsite/spiders/bbc.py
+++ ScrapyNewsWebsite/spiders/bbc.py
@@ -1,21 +1,25 @@
 import scrapy
-import re
-
+import json
 
 class BBCSpider(scrapy.Spider):
     name = "bbc"
-
-    def start_requests(self):
-        url = "https://www.bbc.com/"
-        yield scrapy.Request(url=url, callback=self.parse)
+    start_urls=[]
+    
+    with open('search.json', 'r') as url_list:
+        url_list = json.loads(url_list.read())
+    for element in url_list:
+        start_urls.append(element['url'])
 
     def parse(self, response):
-        news = []
-        items = response.css('li.media-list__item') 
+        page_title = response.css('title::text').get()
+        items = response.css('.gs-c-promo')
         for item in items:
-            news.append({
-                "image_url": item.css('.responsive-image img::attr(src)').get(),
-                "head_line": re.sub("\n", "", str(item.css('.media__link::text').get())),
-                "tag": str(item.css('.media__tag::text').get())
-            })
-        print(f'------------------>>{news[0]}<<-----------------')
+            news = {
+                "page_title":page_title,
+                "head":item.css('.gs-c-promo-heading__title::text').get(),
+                "tag":item.css('.gs-c-section-link span::text').get(),
+                "url_news":item.css('.gs-c-promo-heading::attr(href)').get(),
+                "media_url":item.css('.gs-o-responsive-image img::attr(src)').get(),
+                "date_time":item.css('.gs-o-bullet__text::attr(datetime)').get()
+            }
+            yield news
